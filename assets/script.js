ab4183472d20422b53249bc06c4511abb674dde5
Da-Hubb <josemariaofurum@gmail.com>
2023-06-26 16:18:01 +0100

    bootstrap and jquery

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,24 @@
 $(document).ready(() => {
-    console.log("jose");
+    $(".form").submit(e => {
+        e.preventDefault();
+        let value = $("[name ='email']").val();
+        value && validate(value, "on");
+    })
+
+    $(".dismiss").on({
+        click: () => validate("", "off")
+    })
+
+    function validate(value, condition) {
+        if(condition == "on") {
+            $(".thanks").removeClass("d-none");
+            $(".child-wrap").addClass("d-none");
+            $(".your-email").text(value);
+        }else {
+            $(".thanks").addClass("d-none");
+            $(".child-wrap").removeClass("d-none");
+            $(".your-email").text("");
+            $("[name ='email']").val("")
+        }
+    }
 })
